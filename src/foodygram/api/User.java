ca31e4f4912f52e9b7cf33eb01e69ff1630fdedf
Ethan Chan <ethan.ccj@gmail.com>
2017-04-17 10:04:22 +0800

    added get user/{id}

--- src/foodygram/api/User.java
+++ src/foodygram/api/User.java
@@ -4,14 +4,21 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 @Path("users")
 public class User {
-
+	
 	@GET
 	public Response GetAll() {
 		
+		return Response.ok().build();
+	}
+	
+	@GET
+	@Path("users/{id}")
+	public Response Get(@PathParam(value = "") int id) {
 		return Response.ok().build();
 	}
 	
